faff06f5777e041df9e8ea479d22c1e3125250aa
madwinc <104110214+madwinc@users.noreply.github.com>
2022-04-24 14:16:44 -0500

    Update Program.cs

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                var pgsql = OpenDbConnection("Server=db;Username=postgres_user;Password=postgres_password;");
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                            pgsql = OpenDbConnection("Server=db;Username=postgres_user;Password=postgres_password;");
                         }
                         else
                         { // Normal +1 vote requested
